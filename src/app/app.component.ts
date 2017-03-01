58fcff45142e6b26625dee9a78fd2a9f0af9d108
Marvin Diaz <marvindaviddiaz@hotmail.com>
2017-02-28 20:16:40 -0600

    Animation Callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -138,4 +138,12 @@ export class AppComponent {
     this.list.splice(this.list.indexOf(item), 1);
   }
 
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
+  }
+
 }
